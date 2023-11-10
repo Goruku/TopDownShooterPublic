41aac41603e7613ec42ab33338bcd78c09c121af
Goruku <41842302+Goruku@users.noreply.github.com>
2023-11-10 00:08:34 -0500

    Event based actions

--- Assets/PhysicsMovement/PlayerMover.cs
+++ Assets/PhysicsMovement/PlayerMover.cs
@@ -3,37 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class PlayerMover : MonoBehaviour
 {
     public Rigidbody2D rigidBody2D;
-    public InputActionAsset inputActionAsset;
+    public PlayerActionHub playerActionHub;
 
+    public bool isMoving;
+    [FormerlySerializedAs("stoppedMoving")] public bool shouldStop;
+    public Vector2 velocityVector;
     public float movementSpeed;
-    
-    // Start is called before the first frame update
-    void Start()
+
+    private void OnEnable()
     {
-        
+        playerActionHub.move.AddStarted(StartMovement);
+        playerActionHub.move.AddPerformed(StartMovement);
+        playerActionHub.move.AddCanceled(EndMovement);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        playerActionHub.move.RemoveStarted(StartMovement);
+        playerActionHub.move.RemovePerformed(StartMovement);
+        playerActionHub.move.RemoveCanceled(EndMovement);
+    }
+
+    private void StartMovement(InputAction.CallbackContext callbackContext)
+    {
+        velocityVector = callbackContext.ReadValue<Vector2>().normalized * movementSpeed;
+        isMoving = true;
+    }
+
+    private void EndMovement(InputAction.CallbackContext callbackContext)
+    {
+        velocityVector = new Vector2();
+        isMoving = false;
+        shouldStop = true;
     }
 
     private void FixedUpdate()
     {
+        if (!isMoving && !shouldStop) return;
         if (rigidBody2D.velocity.magnitude > movementSpeed) return;
-        var movementVector = inputActionAsset.actionMaps[0]["Move"].ReadValue<Vector2>().normalized;
-        if (movementVector.magnitude == 0)
-        {
-            rigidBody2D.velocity = new Vector2();
-        }
-        else
-        {
-            rigidBody2D.velocity = movementVector * movementSpeed;
-        }
+        rigidBody2D.velocity = velocityVector.magnitude != 0 ? velocityVector : new Vector2();
+        shouldStop = false;
     }
 }
