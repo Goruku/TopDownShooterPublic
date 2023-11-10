a9e5f9839294cc23ab5bead98099c24d588fd39a
Goruku <41842302+Goruku@users.noreply.github.com>
2023-11-10 00:08:34 -0500

    Better aliasing for PlayerActionHub input actions

--- Assets/ShootingManager.cs
+++ Assets/ShootingManager.cs
@@ -33,7 +33,7 @@ public class ShootingManager : MonoBehaviour
     {
         if (owner is Agent agent)
         {
-            agent.playerActionHub.fire.AddPerformed(AttemptFiring);
+            agent.playerActionHub.fire.performed += AttemptFiring;
         }
     }
 
@@ -41,7 +41,7 @@ public class ShootingManager : MonoBehaviour
     {
         if (owner is Agent agent)
         {
-            agent.playerActionHub.fire.RemovePerformed(AttemptFiring);
+            agent.playerActionHub.fire.performed -= AttemptFiring;
         }
     }
 
